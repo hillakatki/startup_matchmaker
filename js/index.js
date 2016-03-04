ea475584a33678561884c23d6d4864c406117745
hillakatki <hillakatki@gmail.com>
2016-03-03 21:33:50 -0500

    Mobile & Table breakpoints, hamburger menu slide working
    still bumping into things

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("signup").onclick = function (){
+	window.alert('Not taking sign ups yet.');
+};
+
+$('.hamburger').on('click', function() {
+	$('.mobile-nav').slideToggle(2000);
+});
